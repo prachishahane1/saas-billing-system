b3f599d164404d500cb8644e80ff63c5832f7286
prachi <prachishahane1@gmail.com>
2026-06-03 11:26:03 +0530

    Added new file

--- src/main/java/com/triplixtech/saas/repository/userRepository.java
+++
@@ -1,7 +0,0 @@
-package com.triplixtech.saas.repository;
-
-import com.triplixtech.saas.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
